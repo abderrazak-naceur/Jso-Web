a50aedd23fc59175f5d270d8415b13bde76479f6
Abderrazak Naceur <therzeos@gmail.com>
2026-09-24 21:27:26 +0200

    feat(admin): route /admin to protected back office UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AdminApp from './admin/AdminApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {window.location.pathname.startsWith('/admin') ? <AdminApp /> : <App />}
   </StrictMode>,
 )
