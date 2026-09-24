0ddc59b872005118267db8298a63046379bc6983
Abderrazak Naceur <therzeos@gmail.com>
2026-09-24 21:24:00 +0200

    feat(data): bootstrap JSO development data on startup

--- backend/src/JSO.Api/Program.cs
+++ backend/src/JSO.Api/Program.cs
@@ -44,6 +44,14 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<JsoDbContext>();
+    var logger = scope.ServiceProvider.GetRequiredService<ILoggerFactory>().CreateLogger("DevelopmentDataSeeder");
+    await DevelopmentDataSeeder.SeedAsync(db, logger);
+}
+
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
 }
